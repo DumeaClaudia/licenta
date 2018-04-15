a23c82c4b2b1d75beb9d5bc7242b8962dc131bc9
claudia <claudia.dumea18@gmail.com>
2018-04-16 00:01:42 +0300

    The reorganization of some files

---
+++ commons/src/main/java/com/license/Restaurant.java
@@ -0,0 +1,13 @@
+package com.license;
+
+import java.io.Serializable;
+
+public class Restaurant implements Serializable {
+
+    private static final long serialVersionUID = 8913732065557648340L;
+    
+    private String name;
+    private String idAddress;
+    private String idProduct;
+  
+}
