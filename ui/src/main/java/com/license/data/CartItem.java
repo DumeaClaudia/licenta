471f68f3bb6c0b2056fa59e6a939ca75c4e3c069
claudia <claudia.dumea18@gmail.com>
2018-12-16 17:20:33 +0200

    getCartDetails for Android

--- ui/src/main/java/com/license/data/CartItem.java
+++ ui/src/main/java/com/license/data/CartItem.java
@@ -2,13 +2,16 @@ package com.license.data;
 
 public class CartItem {
 	
+	String idCart;
 	String createdDate;
     String restaurantName;
+    String restaurantImage;
     
-	public CartItem( String restaurantName, String createdDate) {
-		super();
+	public CartItem(String idCart, String restaurantName, String createdDate, String restaurantImage) {
+		this.idCart = idCart;
 		this.createdDate = createdDate;
 		this.restaurantName = restaurantName;
+		this.restaurantImage = restaurantImage;
 	}
 
 	public String getCreatedDate() {
@@ -18,6 +21,12 @@ public class CartItem {
 	public String getRestaurantName() {
 		return restaurantName;
 	}
-    
 
+	public String getIdCart() {
+		return idCart;
+	}
+
+	public String getRestaurantImage() {
+		return restaurantImage;
+	}
 }
--- ui/src/main/java/com/license/servlets/GetCartListServlet.java
+++ ui/src/main/java/com/license/servlets/GetCartListServlet.java
@@ -26,12 +26,14 @@ public class GetCartListServlet extends HttpServlet {
 		//String s = request.getQueryString();
 		cartItems.clear();
 		//System.out.println("ID = " + s + " + " + request.getParameter("id"));
-	    cartItems.add(new CartItem("Restaurant1", "12.12.2012"));
-        cartItems.add(new CartItem("Restaurant1", "12.12.2012"));
-        cartItems.add(new CartItem("Restaurant1", "12.12.2012"));
+	    cartItems.add(new CartItem("1", "Restaurant1", "12.12.2012", "3_1.jpg"));
+	    cartItems.add(new CartItem("2", "Restaurant2", "13.12.2012", "fruits.jpg"));
+	    cartItems.add(new CartItem("3", "Restaurant3", "14.12.2012", "3_1.jpg"));
+
+      
 		
 		// get_cart_example e apelat din android....
-		
+        System.out.println("Count = " +cartItems.size());
 	      // Set the response message's MIME type
 	      response.setContentType("application/json");
 	      ObjectMapper mapper = new ObjectMapper();
